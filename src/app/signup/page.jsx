f356786c15f0ec7fd15a90e3941541a3d20147bf
ving <ndvinh360@gmail.com>
2026-01-02 00:07:06 +0700

    feat: Refactor Signup page layout, remove HeaderLayout, and enhance UI components

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import Signup from "@/app/signup/signup";
-import HeaderLayout from "@/components/Layout/HeaderLayout";
 
 export const metadata = {
   title: "Signup",
 };
 
 export default function Page() {
-  return (
-    <HeaderLayout>
-      <Signup />
-    </HeaderLayout>
-  );
+  return <Signup />;
 }
